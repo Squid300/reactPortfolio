149a223dbcf3aa9e1c04baa6b7a6e959c902aa04
Squid300 <squid400@gmail.com>
2022-08-31 23:31:48 -0400

    finished aboutme and updated navs

--- src/PortfolioContainer.js
+++ src/PortfolioContainer.js
@@ -9,13 +9,13 @@ export default function PortfolioContainer() {
 
   // This method is checking to see what the value of `currentPage` is. Depending on the value of currentPage, we return the corresponding component to render.
   const renderPage = () => {
-    if (currentPage === 'About') {
-      return <Home />;
-    }
     if (currentPage === 'Contact') {
       return <Contact />;
     }
-    return <Portfolio />;
+    if (currentPage === 'Portfolio') {
+      return <Portfolio />;
+    }
+    return <Home />;
   };
 
   const handlePageChange = (page) => setCurrentPage(page);
--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -2,10 +2,12 @@ import React from 'react';
 
 export default function About() {
     return (
-    <div>
-        <h1>About Page</h1>
+    <div className='card'>
+        <h2>Ben Anderson</h2>
+        <img src="./imgs/profilePic.jpg" style={{height: '100px', width: '75px'}} alt="profilePic"/>
         <p>
-          Full Stack web developing.
+          Full Stack web developer. 
+          While profictent in most areas my specialty is working with databases.
         </p>
     </div>
     );
